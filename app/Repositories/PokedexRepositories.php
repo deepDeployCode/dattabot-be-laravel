e48e09d873b908a915fc49e5868005c2bb52b4da
syamsulapp <syamsulmar_f1b118038@student.uho.ac.id>
2023-12-11 20:13:53 +0800

    add filter of list data pokemon

--- app/Repositories/PokedexRepositories.php
+++ app/Repositories/PokedexRepositories.php
@@ -19,7 +19,26 @@ trait PokedexRepositories
         $data = Pokedex_dataset::orderBy($request->sort ? $request->sort : 'id', $request->typeSort ? $request->typeSort : 'ASC')
             ->when($request->name, function ($query) use ($request) {
                 return $query->where('name', 'like', "%{$request->name}%");
-            })->paginate($this->response()->limit($request));
+            })
+            ->when($request->pokedex_number, function ($query) use ($request) {
+                return $query->where('pokedex_number', 'like', "%{$request->pokedex_number}%");
+            })
+            ->when($request->german_name, function ($query) use ($request) {
+                return $query->where('german_name', 'like', "%{$request->german_name}%");
+            })
+            ->when($request->japanese_name, function ($query) use ($request) {
+                return $query->where('japanese_name', 'like', "%{$request->japanese_name}%");
+            })
+            ->when($request->generation, function ($query) use ($request) {
+                return $query->where('generation', 'like', "%{$request->generation}%");
+            })
+            ->when($request->status, function ($query) use ($request) {
+                return $query->where('status', 'like', "%{$request->status}%");
+            })
+            ->when($request->species, function ($query) use ($request) {
+                return $query->where('species', 'like', "%{$request->species}%");
+            })
+            ->paginate($this->response()->limit($request));
 
         return ListPokedexResource::collection($data);
     }
